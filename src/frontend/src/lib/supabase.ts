23ff852c954efbb5f8ddae9a04ec46da459a6a59
Zidan <zidanvai111@gmail.com>
2026-06-08 06:33:18 +0600

    Move Supabase config to environment variables

--- src/frontend/src/lib/supabase.ts
+++ src/frontend/src/lib/supabase.ts
@@ -1,10 +1,7 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl =
-"https://uigazwsnwyhmwoqglxbp.supabase.co"
-
-const supabaseAnonKey =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVpZ2F6d3Nud3lobXdvcWdseGJwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODA1NzkyNzIsImV4cCI6MjA5NjE1NTI3Mn0.5uFWchb_9asQ5BgBUpUeGH62EGnPwhiHUUuUZ7KY3FE";
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
 export const supabase = createClient(
   supabaseUrl,
